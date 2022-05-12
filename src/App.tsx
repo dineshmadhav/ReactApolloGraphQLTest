d19ca6b843cf63ea49db54cfb2966e3b83d35f8d
computer <computer@computers-MacBook-Pro.local>
2022-05-12 14:29:09 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useQuery, gql } from "@apollo/client";
 import logo from './logo.svg';
 import './App.css';
 
+const FILMS_QUERY = gql`
+  {
+    launchesPast(limit: 10) {
+      id
+      mission_name
+    }
+  }
+`;
+
+interface launchObject { id: React.Key | null | undefined; mission_name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }
+
 function App() {
+  const { data, loading, error } = useQuery(FILMS_QUERY);
+
+  if (loading) return <div>"Loading..."</div>;
+  if (error) return <pre>{error.message}</pre>
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +34,14 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+    <h1>SpaceX Launches</h1>
+      <ul>
+        {data.launchesPast.map((launch: launchObject) => (
+          <li key={launch.id}>{launch.mission_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache()
+});
+
+const rootElement = document.getElementById("root") as HTMLElement;
+ReactDOM.render(
   <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
+  </React.StrictMode>,
+  rootElement
 );
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
